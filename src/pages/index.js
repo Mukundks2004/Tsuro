1e58b468ee88eac2bbb721b210d402b8031e0d1f
Mks <mukundsrinivasan0@gmail.com>
2024-10-05 17:45:32 +1000

    deleted bad files, unused code

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,25 +4,4 @@ function gotoGame() {
 
 const messageElement = document.getElementById('messagePlace');
 
-const fetchHello = async () => {
-    const response = await fetch('http://localhost:3000/hello');
-    const data = await response.json();
-    messageElement.textContent = data.message;
-};
-
-const fetchGoodbye = async () => {
-    const response = await fetch('http://localhost:3000/goodbye');
-    const data = await response.json();
-    messageElement.textContent = data.message;
-};
-
-const fetchWow = async () => {
-    const response = await fetch('http://localhost:3000/wow');
-    const data = await response.json();
-    messageElement.textContent = data.message;
-};
-
 document.getElementById("gotoGamePage").addEventListener("click", gotoGame);
-document.getElementById("getHello").addEventListener("click", fetchHello);
-document.getElementById("getGoodbye").addEventListener("click", fetchGoodbye);
-document.getElementById("getWow").addEventListener("click", fetchWow);
